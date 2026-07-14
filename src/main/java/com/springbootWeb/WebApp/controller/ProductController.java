eb5de6d895b22503b2d1c03ffac0fb4e19441b7d
Akash Sharma <akash.sharma@arrise.com>
2026-07-14 11:48:19 +0530

    Update: Productcontroller and product class updated

--- src/main/java/com/springbootWeb/WebApp/controller/ProductController.java
+++ src/main/java/com/springbootWeb/WebApp/controller/ProductController.java
@@ -1,9 +1,22 @@
 package com.springbootWeb.WebApp.controller;
 
+import com.springbootWeb.WebApp.model.Product;
+import com.springbootWeb.WebApp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
 
-    public String getProducts(){
-        return "";
+    @Autowired
+    ProductService service;
+
+    @RequestMapping("/products")
+    public List<Product> getProducts(){
+        return service.getProducts();
     }
 
 }
