2e82e986dfa391685943a29f274b3d9b3fa1d8f0
Akash Sharma <akash.sharma@arrise.com>
2026-07-14 13:25:52 +0530

    Update: Added GET Api for prodid

--- src/main/java/com/springbootWeb/WebApp/controller/ProductController.java
+++ src/main/java/com/springbootWeb/WebApp/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.springbootWeb.WebApp.controller;
 import com.springbootWeb.WebApp.model.Product;
 import com.springbootWeb.WebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,11 @@ public class ProductController {
     @RequestMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+
+    @RequestMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId) {
+        return service.getProductById(prodId);
     }
 
 }
